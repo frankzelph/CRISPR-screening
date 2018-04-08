eb0470bfe5e150e187e79066c1441ec51c829a69
frankzelph <fzelph@gmail.com>
2018-04-08 18:55:47 +0800

    Add files via upload

--- bowtie.sh
+++ bowtie.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+# Map the sgRNA reads to library reference using Bowtie
+#
+# Database directory
+db_dir=/home/sucsb/database/mouse_geckov2/bowtie_index
 
-# 1A
-bowtie ~/database/GeCKOv2/bowtie_index/libA -f 1A-NGS_trimed_R1.fasta -m 1 -v 1 -norc -p 24 -S 1A.sam
-# 2A
-bowtie ~/database/GeCKOv2/bowtie_index/libA -f 2A-NGS_trimed_R1.fasta -m 1 -v 1 -norc -p 24 -S 2A.sam
-# 1AD
-bowtie ~/database/GeCKOv2/bowtie_index/libA -f 1AD-NGS_trimed_R1.fasta -m 1 -v 1 -norc -p 24 -S 1AD.sam
-# 2AD
-bowtie ~/database/GeCKOv2/bowtie_index/libA -f 2AD-NGS_trimed_R1.fasta -m 1 -v 1 -norc -p 24 -S 2AD.sam
-# 1B-1
-bowtie ~/database/GeCKOv2/bowtie_index/libB -f 1B-1_trimed_R1.fasta -m 1 -v 1 -norc -p 24 -S 1B-1.sam
-# 2B-1
-bowtie ~/database/GeCKOv2/bowtie_index/libB -f 2B-1_trimed_R1.fasta -m 1 -v 1 -norc -p 24 -S 2B-1.sam
+for item in `ls `
+do
+	if [ -f $item ] && [[ $item == *"fastq" ]]; then
+		bowtie ${db_dir}/libAB -q $item -m 1 -v 0 -norc -p 32 -S ${item%%.*}.sam
+	fi
+done
--- cutadapt.sh
+++ cutadapt.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+# Use cutadapt to trim both 5' and 3' ends of sgRNA spacer sequences.
 
-# 1AD-NGS
-cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/1AD-NGS_trimed_R1.fasta 1AD-NGS_combined_R1.fastq.gz
-# 2AD-NGS
-cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/2AD-NGS_trimed_R1.fasta 2AD-NGS_combined_R1.fastq.gz
-# 1A
-cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/1A-NGS_trimed_R1.fasta 1A-NGS_combined_R1.fastq.gz
-# 2A
-cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/2A-NGS_trimed_R1.fasta 2A-NGS_combined_R1.fastq.gz
-# 1B-1
-cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/1B-1_trimed_R1.fasta 1B-1_combined_R1.fastq.gz
-#2B-1
-cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/2B-1_trimed_R1.fasta 2B-1_combined_R1.fastq.gz
+if [ ! -d "trimed_seqs" ]; then
+	echo "Make a directory named \"trimed_seqs\"."
+	mkdir "trimed_seqs"
+fi
 
-
-
-# cutadapt -j 24 -g -m 20 TCTTGTGGAAAGGACGAAACACCG...gttttagagctagaaatagcaagtta --discard-untrimmed -o trimed_seq/1AD-NGS_trimed_R1.fastq -p trimed_seq/1AD-NGS_trimed_R2.fastq 1AD-NGS_combined_R1.fastq.gz 1AD-NGS_combined_R2.fastq.gz
+for item in `ls `
+do
+	if [ -f "$item" ] && [[ $item == *"fastq" ]]; then
+		cutadapt -j 32 -m 20 -g tatatatcttgtggaaaggacgaaacaccg...gttttagagctagaaatagc --discard-untrimmed -o trimed_seqs/${item%%.*}_trimed.fastq $item
+	fi
+done
